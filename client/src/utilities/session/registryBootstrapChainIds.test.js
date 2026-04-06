dfbb754b062faf39c85ac8530ac349fa50f5a74a
Agalmic <agalmicsoftware@protonmail.com>
2026-04-06 10:52:52 -0700

    refactor: replace hardcoded chain ID with DEFAULT_CHAIN_ID in tests and scripts

--- client/src/utilities/session/registryBootstrapChainIds.test.js
+++ client/src/utilities/session/registryBootstrapChainIds.test.js
@@ -1,51 +1,55 @@
+import { DEFAULT_CHAIN_ID } from '../../variables/appConfig.js';
 import { resolveSessionRegistryBootstrapChainIds } from './registryBootstrapChainIds.js';
+
+const CONFIGURED_DEFAULT_CHAIN_ID = DEFAULT_CHAIN_ID;
+const OTHER_CHAIN_ID = CONFIGURED_DEFAULT_CHAIN_ID === 84532 ? 8453 : 84532;
 
 describe('resolveSessionRegistryBootstrapChainIds', () => {
   it('pins empty list scope to the active or default chain', () => {
     expect(resolveSessionRegistryBootstrapChainIds({
       scope: 'list',
       list: [],
-      activeChainId: 11155420,
-      defaultChainId: 84532,
-    })).toEqual([11155420]);
+      activeChainId: CONFIGURED_DEFAULT_CHAIN_ID,
+      defaultChainId: OTHER_CHAIN_ID,
+    })).toEqual([CONFIGURED_DEFAULT_CHAIN_ID]);
 
     expect(resolveSessionRegistryBootstrapChainIds({
       scope: 'list',
       list: [],
       activeChainId: 0,
-      defaultChainId: 11155420,
-    })).toEqual([11155420]);
+      defaultChainId: CONFIGURED_DEFAULT_CHAIN_ID,
+    })).toEqual([CONFIGURED_DEFAULT_CHAIN_ID]);
   });
 
   it('treats general-only list scope as a single-chain bootstrap', () => {
     expect(resolveSessionRegistryBootstrapChainIds({
       scope: 'list',
       list: ['general', ''],
-      activeChainId: 11155420,
-      defaultChainId: 84532,
-    })).toEqual([11155420]);
+      activeChainId: CONFIGURED_DEFAULT_CHAIN_ID,
+      defaultChainId: OTHER_CHAIN_ID,
+    })).toEqual([CONFIGURED_DEFAULT_CHAIN_ID]);
   });
 
   it('allows all-chain fanout only when scope explicitly needs broad registry coverage', () => {
     expect(resolveSessionRegistryBootstrapChainIds({
       scope: 'list',
       list: ['edge-session'],
-      activeChainId: 11155420,
-      defaultChainId: 84532,
+      activeChainId: CONFIGURED_DEFAULT_CHAIN_ID,
+      defaultChainId: OTHER_CHAIN_ID,
     })).toBeUndefined();
 
     expect(resolveSessionRegistryBootstrapChainIds({
       scope: 'all',
       list: [],
-      activeChainId: 11155420,
-      defaultChainId: 84532,
+      activeChainId: CONFIGURED_DEFAULT_CHAIN_ID,
+      defaultChainId: OTHER_CHAIN_ID,
     })).toBeUndefined();
 
     expect(resolveSessionRegistryBootstrapChainIds({
       scope: 'active',
       list: [],
-      activeChainId: 11155420,
-      defaultChainId: 84532,
+      activeChainId: CONFIGURED_DEFAULT_CHAIN_ID,
+      defaultChainId: OTHER_CHAIN_ID,
       forceAllChains: true,
     })).toBeUndefined();
   });
