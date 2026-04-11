88078a5b5557b62a962aa08f764568abb12ab963
Agalmic <agalmicsoftware@protonmail.com>
2026-04-10 23:43:30 -0700

    feat: update GitHub links to point to the new AI discourse corpus location and refactor URL handling

--- client/src/variables/publicRepoMetadata.js
+++ client/src/variables/publicRepoMetadata.js
@@ -6,10 +6,17 @@ export const PUBLIC_SECURITY_EMAIL = 'agalmicsoftware@protonmail.com';
 export const PUBLIC_REPO_URL = `https://github.com/${PUBLIC_GITHUB_ORG}/${PUBLIC_GITHUB_REPO}`;
 export const PUBLIC_REPO_ISSUES_URL = `${PUBLIC_REPO_URL}/issues`;
 export const PUBLIC_REPO_NEW_ISSUE_URL = `${PUBLIC_REPO_ISSUES_URL}/new`;
-export const PUBLIC_CONTRIBUTING_URL = `${PUBLIC_REPO_URL}/blob/${PUBLIC_GITHUB_BRANCH}/CONTRIBUTING.md`;
-export const PUBLIC_LICENSE_URL = `${PUBLIC_REPO_URL}/blob/${PUBLIC_GITHUB_BRANCH}/LICENSE`;
-export const PUBLIC_WHITEPAPER_URL = `${PUBLIC_REPO_URL}/blob/${PUBLIC_GITHUB_BRANCH}/Whitepaper/whitepaper.md`;
-export const PUBLIC_CONTRACTS_SOURCE_BASE_URL = `${PUBLIC_REPO_URL}/blob/${PUBLIC_GITHUB_BRANCH}/contracts`;
+export const buildPublicRepoBlobUrl = (pathname = '') => (
+  pathname ? `${PUBLIC_REPO_URL}/blob/${PUBLIC_GITHUB_BRANCH}/${String(pathname).replace(/^\/+/, '')}` : ''
+);
+export const buildPublicRepoTreeUrl = (pathname = '') => (
+  pathname ? `${PUBLIC_REPO_URL}/tree/${PUBLIC_GITHUB_BRANCH}/${String(pathname).replace(/^\/+/, '')}` : ''
+);
+export const PUBLIC_CONTRIBUTING_URL = buildPublicRepoBlobUrl('CONTRIBUTING.md');
+export const PUBLIC_LICENSE_URL = buildPublicRepoBlobUrl('LICENSE');
+export const PUBLIC_WHITEPAPER_URL = buildPublicRepoBlobUrl('Whitepaper/whitepaper.md');
+export const PUBLIC_CONTRACTS_SOURCE_BASE_URL = buildPublicRepoBlobUrl('contracts');
+export const PUBLIC_AI_DISCOURSE_CORPUS_URL = buildPublicRepoTreeUrl('ai-discourse-corpus');
 
 export const buildPublicContractSourceUrl = (filename = '') => (
   filename ? `${PUBLIC_CONTRACTS_SOURCE_BASE_URL}/${filename}` : ''
