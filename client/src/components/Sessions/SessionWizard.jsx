0ffbdef41c7e65655af2a48db5930d05bd5bb6c4
Agalmic <agalmicsoftware@protonmail.com>
2026-04-14 12:53:49 -0700

    test: update SessionWizard tests to verify toggleLoginModal calls with correct arguments

--- client/src/components/Sessions/SessionWizard.jsx
+++ client/src/components/Sessions/SessionWizard.jsx
@@ -4036,6 +4036,7 @@ const SessionWizard = ({
 
   useEffect(() => {
     if (!toStr(account).trim() || !pendingCreateSbtLaunch) return;
+    if (typeof toggleLoginModal === 'function') toggleLoginModal(false);
     openCreateSbtModal(pendingCreateSbtLaunch);
     setPendingCreateSbtLaunch(null);
   // eslint-disable-next-line react-hooks/exhaustive-deps
