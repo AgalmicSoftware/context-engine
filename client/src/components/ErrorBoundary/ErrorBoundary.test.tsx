3a619a16d50f810dd3a2615e353795d453d9affd
Agalmic <agalmicsoftware@protonmail.com>
2026-04-22 19:09:10 -0700

    refactor(autocoder): type AppErrorBoundary

--- client/src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ client/src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import AppErrorBoundary from './AppErrorBoundary.jsx';
+import AppErrorBoundary from './AppErrorBoundary';
 import RouteErrorBoundary from './RouteErrorBoundary.jsx';
 
 const Thrower = () => {
