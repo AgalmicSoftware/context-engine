0fa2c6752ead90fee1c04643f9d8aa0bedb4b016
Agalmic <agalmicsoftware@protonmail.com>
2026-04-24 21:08:42 -0700

    refactor(autocoder): type leaf utility helpers

--- client/src/utilities/survey/freeformAnswerUtils.js
+++ client/src/utilities/survey/freeformAnswerUtils.js
@@ -1,12 +1,1 @@
-const normalizeQuestionType = (questionType) => {
-  if (typeof questionType !== 'string') return '';
-  return questionType.trim().toLowerCase();
-};
-
-export const isFreeformBlankAnswer = (questionType, parsedResponse) => {
-  const normalizedQuestionType = normalizeQuestionType(questionType);
-  if (!normalizedQuestionType) return false;
-  if (normalizedQuestionType !== 'freeform') return false;
-  const answerValue = parsedResponse?.answer?.value;
-  return typeof answerValue === 'string' && answerValue.trim() === '';
-};
+export * from './freeformAnswerUtils.ts';
