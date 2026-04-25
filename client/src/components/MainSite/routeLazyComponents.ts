aa8554b870a634e0727b7100ad3c04d8f045a54e
Agalmic <agalmicsoftware@protonmail.com>
2026-04-24 21:13:37 -0700

    refactor(autocoder): type contract and onboarding helper modules

---
+++ client/src/components/MainSite/routeLazyComponents.ts
@@ -0,0 +1,24 @@
+/** @module routeLazyComponents */
+
+import React from 'react';
+
+export const SurveyPage = React.lazy(() => import('../SurveyTool/SurveyPage'));
+export const SurveyTool = React.lazy(() => import('../SurveyTool/SurveyTool.jsx'));
+export const SBTPage = React.lazy(() => import('../SBTs/SBTPage'));
+export const SBTsPage = React.lazy(() => import('../SBTs/SBTsList')); // SBTsList, often referred to as SBTsPage in comments
+export const DebateMap = React.lazy(() => import('../DebateMap/DebateMap'));
+export const AdminPage = React.lazy(() => import('../Admin/AdminPage'));
+export const SponsorPage = React.lazy(() => import('../Sponsor/SponsorPage'));
+export const SessionWizard = React.lazy(() => import('../Sessions/SessionWizard'));
+export const OnePageSession = React.lazy(() => import('../OnePageSession/OnePageSession'));
+export const DemosIndex = React.lazy(() => import('../DemoViews/DemosIndex'));
+export const SessionDocumentsPage = React.lazy(() => import('../DocumentLibrary/SessionDocumentsPage'));
+export const SimulatedUserPage = React.lazy(() => import('../UserPage/SimUserPage'));
+export const UserPage = React.lazy(() => import('../UserPage/UserPage'));
+export const CompareAddresses = React.lazy(() => import('../UserPage/CompareAddresses'));
+export const ContractPage = React.lazy(() => import('../ContractPage/ContractPage'));
+export const BookmarksPage = React.lazy(() => import('../Bookmarks/BookmarksPage'));
+export const RiskMatrixDemo = React.lazy(() => import('../DemoViews/RiskMatrixDemo'));
+export const AboutPage = React.lazy(() => import('../About/AboutPage'));
+export const AgentPage = React.lazy(() => import('../Agent/AgentPage'));
+export const TagPage = React.lazy(() => import('../TagPage/TagPage'));
