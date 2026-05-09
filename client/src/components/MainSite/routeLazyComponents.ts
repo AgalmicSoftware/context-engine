1688204661ddd01671168a99bee49520fa1a27f6
Agalmic <agalmicsoftware@protonmail.com>
2026-05-09 14:51:17 -0700

    Revert "feat(autocoder): add agent activity inbox"
    This reverts commit 184c7ea87dda2010b82003b3ef7ea40dc2e88d22.
    
    CE-Private-Source: a91a9e6b2476a56388860a86361dc0965b1a46a3

--- client/src/components/MainSite/routeLazyComponents.ts
+++ client/src/components/MainSite/routeLazyComponents.ts
@@ -23,6 +23,5 @@ export const RiskMatrixDemo = React.lazy(() => import('../DemoViews/RiskMatrixDe
 export const AboutPage = React.lazy(() => import('../About/AboutPage'));
 export const PostsPage = React.lazy(() => import('../Posts/PostsPage'));
 export const AgentPage = React.lazy(() => import('../Agent/AgentPage'));
-export const AgentInboxPage = React.lazy(() => import('../Agent/AgentInboxPage'));
 export const TelegramDemoSetupPage = React.lazy(() => import('../TelegramDemoSetup/TelegramDemoSetupPage'));
 export const TagPage = React.lazy(() => import('../TagPage/TagPage'));
