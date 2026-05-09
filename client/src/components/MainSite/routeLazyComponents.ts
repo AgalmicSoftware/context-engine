65d311d78302dd9dc8670b01f555fcac36a2682a
Agalmic <agalmicsoftware@protonmail.com>
2026-05-09 11:42:55 -0700

    feat(autocoder): add agent activity inbox

--- client/src/components/MainSite/routeLazyComponents.ts
+++ client/src/components/MainSite/routeLazyComponents.ts
@@ -21,5 +21,6 @@ export const BookmarksPage = React.lazy(() => import('../Bookmarks/BookmarksPage
 export const RiskMatrixDemo = React.lazy(() => import('../DemoViews/RiskMatrixDemo'));
 export const AboutPage = React.lazy(() => import('../About/AboutPage'));
 export const AgentPage = React.lazy(() => import('../Agent/AgentPage'));
+export const AgentInboxPage = React.lazy(() => import('../Agent/AgentInboxPage'));
 export const TelegramDemoSetupPage = React.lazy(() => import('../TelegramDemoSetup/TelegramDemoSetupPage'));
 export const TagPage = React.lazy(() => import('../TagPage/TagPage'));
