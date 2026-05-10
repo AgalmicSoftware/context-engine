11d236f434e1b510bd35590e35e5b3eb2b266120
Agalmic <agalmicsoftware@protonmail.com>
2026-05-10 11:59:26 -0700

    chore(autocoder): split client production dependencies

--- client/src/utilities/tooling/clientPackageContract.test.js
+++ client/src/utilities/tooling/clientPackageContract.test.js
@@ -20,8 +20,31 @@ describe('client package modernization contract', () => {
     const pkg = readClientPackageJson();
 
     expect(pkg.dependencies.ethers).toBe('5.7.2');
-    expect(pkg.dependencies['react-scripts']).toBe('^4.0.3');
-    expect(pkg.dependencies.webpack).toBe('4.44.2');
+    expect(pkg.devDependencies['react-scripts']).toBe('^4.0.3');
+    expect(pkg.devDependencies.webpack).toBe('4.44.2');
     expect(pkg.overrides.webpack).toBe('4.44.2');
+  });
+
+  it('keeps build, test, lint, analyze, and static serving tools out of production dependencies', () => {
+    const pkg = readClientPackageJson();
+    const devOnlyPackages = [
+      '@babel/core',
+      'babel-jest',
+      'eslint-plugin-prettier',
+      'node-polyfill-webpack-plugin',
+      'raw-loader',
+      'react-scripts',
+      'sass',
+      'sass-loader',
+      'serve',
+      'source-map-explorer',
+      'source-map-loader',
+      'webpack',
+    ];
+
+    devOnlyPackages.forEach((name) => {
+      expect(pkg.dependencies[name]).toBeUndefined();
+      expect(pkg.devDependencies[name]).toBeDefined();
+    });
   });
 });
