6c1fd70fdb64a90971fbc7a1d47801392db09b07
Agalmic <agalmicsoftware@protonmail.com>
2026-05-14 19:19:30 -0700

    fix(autocoder): pin jspdf security update

--- client/src/components/PolisReport/PolisReport.cache.test.js
+++ client/src/components/PolisReport/PolisReport.cache.test.js
@@ -3,6 +3,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import PolisReport, {
   applyFilterStateToAggregator,
   buildClusterAnalysisDataKey,
+  buildPolisReportPdfFilename,
   buildPrecomputedDemoClusterState,
   buildRatingMatrixFromRealData,
   buildRatingMatrixFromDemo,
