2019733c9ea61e6476a9afb76985bc68566c6bcc
Agalmic <agalmicsoftware@protonmail.com>
2026-05-15 14:39:03 -0700

    fix: abort stale mint sends

--- client/src/components/SurveyTool/SurveyQuestions.tsx
+++ client/src/components/SurveyTool/SurveyQuestions.tsx
@@ -669,15 +669,9 @@ export class SurveyQuestions extends Component<SurveyQuestionsProps, SurveyQuest
     try {
       return this.setState(next, (...args) => {
         try {
-          const result = typeof callback === 'function' ? callback(...args) : undefined;
-          if (result && typeof result.then === 'function') {
-            return result.finally(release);
-          }
+          return typeof callback === 'function' ? callback(...args) : undefined;
+        } finally {
           release();
-          return result;
-        } catch (error) {
-          release();
-          throw error;
         }
       });
     } catch (error) {
