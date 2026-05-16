4c08652798717e7f27bbc190b1a4ae33db3bae98
Agalmic <agalmicsoftware@protonmail.com>
2026-05-15 19:40:57 -0700

    refactor: remove stale hook suppressions

--- client/src/components/Agent/AgentPage.tsx
+++ client/src/components/Agent/AgentPage.tsx
@@ -86,7 +86,6 @@ export default function AgentPage() {
     } catch (_) {
       return null;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [agent]);
   const actionLabels = Array.isArray(agentContract?.actions)
     ? agentContract.actions
