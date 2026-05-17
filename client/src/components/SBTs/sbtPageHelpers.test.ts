c9988c0f992ebbf3bc0d34a9c3b3a6c44b0d801f
Agalmic <agalmicsoftware@protonmail.com>
2026-05-16 21:32:06 -0700

    test: split sbt page holder state coverage

--- client/src/components/SBTs/sbtPageHelpers.test.ts
+++ client/src/components/SBTs/sbtPageHelpers.test.ts
@@ -27,7 +27,6 @@ import {
   buildSbtPageEncryptedEnvelopeFingerprint,
   buildSbtPageErrorPatch,
   buildSbtPageExportFormatPatch,
-  buildSbtPageHolderListSignature,
   buildSbtPageInitialState,
   buildSbtPageIncludePreviousPasswordsPatch,
   buildSbtPageNetworkUpdatePatch,
@@ -35,8 +34,6 @@ import {
   buildSbtPageIntervalIdPatch,
   buildSbtPageLoadingMintersBurnersPatch,
   buildSbtPageLogScanProgressPatch,
-  buildSbtPageLocalBurnSuccessPatch,
-  buildSbtPageLocalMintSuccessPatch,
   buildSbtPageModalFilteredMintedUsersPatch,
   buildSbtPageMintFailurePatch,
   buildSbtPageMintedModalInitialFilterPatch,
@@ -56,8 +53,6 @@ import {
   buildSbtPagePasswordClaimStartSuccessPatch,
   buildSbtPagePasswordGenerationCountPatch,
   buildSbtPagePasswordMintInputPatch,
-  buildSbtPageAccountDerivedStatePatch,
-  buildSbtPagePrimaryMetadataStatePatch,
   buildSbtPageRefreshOptions,
   buildSbtPageRelevantInfoPatch,
   buildSbtPageResolvedSessionSlugPatch,
@@ -104,7 +99,6 @@ import {
   resolveSbtPageRecoveryCacheChainId,
   resolveSbtPageShouldRefreshCounts,
   resolveSbtPageUrlAutoMintIntent,
-  resolveSbtPageUserAdminStatus,
   resolveSbtAddress,
   resolveSbtAddressString,
   resolveSbtChainId,
@@ -1049,165 +1043,4 @@ describe('sbtPageHelpers', () => {
       ownerLookupUpperBound: null,
     })).toBeNaN();
   });
-
-  it('builds primary metadata state patches with admin status', () => {
-    expect(resolveSbtPageUserAdminStatus({
-      account: '0xAdmin',
-      sbtInfo: { admin: '0xadmin' },
-    })).toBe(true);
-    expect(resolveSbtPageUserAdminStatus({
-      account: '0xAdmin',
-      sbtInfo: { admin_: '0xother' },
-    })).toBe(false);
-    expect(resolveSbtPageUserAdminStatus({
-      account: '',
-      sbtInfo: { admin: '0xadmin' },
-    })).toBe('');
-    const nextInfo = { admin: '0xAdmin' };
-    expect(buildSbtPagePrimaryMetadataStatePatch({
-      account: '0xadmin',
-      extraState: { loadingMintersBurners: false },
-      nextSbtInfo: nextInfo,
-      prevSbtInfo: { stale: true },
-    })).toEqual({
-      sbtInfo: nextInfo,
-      userIsSbtAdmin: true,
-      loadingMintersBurners: false,
-    });
-    expect(buildSbtPagePrimaryMetadataStatePatch({
-      account: '0xadmin',
-      nextSbtInfo: { admin_: '0xOther' },
-      prevSbtInfo: { stale: true },
-    })).toEqual({
-      sbtInfo: { admin_: '0xOther' },
-      userIsSbtAdmin: false,
-    });
-    expect(buildSbtPagePrimaryMetadataStatePatch({
-      account: '0xadmin',
-      nextSbtInfo: null,
-      prevSbtInfo: { stale: true },
-    })).toEqual({
-      sbtInfo: { stale: true },
-      userIsSbtAdmin: '',
-    });
-  });
-
-  it('builds account-derived holder and admin state patches', () => {
-    const holder = '0x00000000000000000000000000000000000000aa';
-    const other = '0x00000000000000000000000000000000000000bb';
-
-    expect(buildSbtPageAccountDerivedStatePatch({
-      account: holder,
-      state: {
-        mintedAddresses: [holder, holder, other],
-        burnedAddresses: [holder],
-        sbtInfo: { admin_: holder },
-        userHasSBT: false,
-        userIsSbtAdmin: false,
-      },
-    })).toEqual({
-      userHasSBT: true,
-      userIsSbtAdmin: true,
-    });
-
-    expect(buildSbtPageAccountDerivedStatePatch({
-      account: holder,
-      state: {
-        mintedAddresses: [holder],
-        burnedAddresses: [],
-        sbtInfo: { admin: other },
-        userHasSBT: true,
-        userIsSbtAdmin: false,
-      },
-    })).toBeNull();
-
-    expect(buildSbtPageAccountDerivedStatePatch({
-      account: '',
-      state: {
-        mintedAddresses: [holder],
-        burnedAddresses: [],
-        sbtInfo: { admin: holder },
-        userHasSBT: true,
-        userIsSbtAdmin: true,
-      },
-    })).toEqual({
-      userHasSBT: false,
-      userIsSbtAdmin: '',
-    });
-  });
-
-  it('builds local mint success holder patches', () => {
-    const holder = '0x00000000000000000000000000000000000000aa';
-    const other = '0x00000000000000000000000000000000000000bb';
-
-    expect(buildSbtPageLocalMintSuccessPatch({
-      addrLower: holder,
-      prevState: {
-        mintedAddresses: [other],
-        burnedAddresses: [holder, other],
-      },
-    })).toEqual({
-      mintedAddresses: [other, holder],
-      burnedAddresses: [other],
-      userHasSBT: true,
-    });
-
-    expect(buildSbtPageLocalMintSuccessPatch({
-      addrLower: '',
-      prevState: {
-        mintedAddresses: [other],
-        burnedAddresses: [holder],
-      },
-    })).toBeNull();
-  });
-
-  it('builds local burn success holder patches', () => {
-    const holder = '0x00000000000000000000000000000000000000aa';
-    const other = '0x00000000000000000000000000000000000000bb';
-
-    expect(buildSbtPageLocalBurnSuccessPatch({
-      addrLower: holder,
-      prevState: {
-        mintedAddresses: [holder, other],
-        burnedAddresses: [],
-        filteredMintedUsers: [holder, other],
-        showModal: true,
-      },
-    })).toEqual({
-      burnedAddresses: [holder],
-      userHasSBT: false,
-      filteredMintedUsers: [other],
-      filteredMintedUsersSignature: buildSbtPageHolderListSignature([other]),
-    });
-
-    expect(buildSbtPageLocalBurnSuccessPatch({
-      addrLower: holder,
-      prevState: {
-        mintedAddresses: [holder],
-        burnedAddresses: [],
-        filteredMintedUsers: [holder],
-        filteredMintedUsersSignature: 'prev',
-      },
-    })).toEqual({
-      burnedAddresses: [holder],
-      userHasSBT: false,
-      filteredMintedUsers: [],
-      filteredMintedUsersSignature: buildSbtPageHolderListSignature([]),
-    });
-
-    expect(buildSbtPageLocalBurnSuccessPatch({
-      addrLower: holder,
-      prevState: {
-        mintedAddresses: [holder],
-        burnedAddresses: [],
-        filteredMintedUsersSignature: 'prev',
-      },
-    })).toEqual({
-      burnedAddresses: [holder],
-      userHasSBT: false,
-      filteredMintedUsers: [],
-      filteredMintedUsersSignature: 'prev',
-    });
-  });
-
 });
