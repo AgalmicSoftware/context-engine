e2259c83b338290a416b188ca6b278556c522c2e
Agalmic <agalmicsoftware@protonmail.com>
2026-05-17 19:55:28 -0700

    test: centralize memory router future flags

--- client/src/components/DemoViews/CorpusViewer.test.tsx
+++ client/src/components/DemoViews/CorpusViewer.test.tsx
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import React from 'react';
 import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { TestMemoryRouter as MemoryRouter } from 'testUtils/TestMemoryRouter';
 
 import CorpusViewer from './CorpusViewer';
 import PolicyGlobe from './PolicyGlobe';
---
+++ client/src/testUtils/TestMemoryRouter.tsx
@@ -0,0 +1,18 @@
+import { MemoryRouter, type MemoryRouterProps } from 'react-router-dom';
+
+export const TEST_ROUTER_FUTURE_FLAGS: NonNullable<MemoryRouterProps['future']> = {
+  v7_relativeSplatPath: true,
+  v7_startTransition: true,
+};
+
+export function TestMemoryRouter({
+  future,
+  ...props
+}: MemoryRouterProps) {
+  return (
+    <MemoryRouter
+      {...props}
+      future={{ ...TEST_ROUTER_FUTURE_FLAGS, ...future }}
+    />
+  );
+}
