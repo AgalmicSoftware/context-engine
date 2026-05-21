13288c36e486b5153d1eccd6ba98a36bd1c261de
Agalmic <agalmicsoftware@protonmail.com>
2026-05-20 20:11:15 -0700

    fix: preserve Vite global style loading

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { initLogging } from 'utilities/logging.js';
-import 'assets/css/contextEngine.scss';
 import App from 'components/App';
 
 initLogging({ showGuide: process.env.NODE_ENV !== 'production' });
