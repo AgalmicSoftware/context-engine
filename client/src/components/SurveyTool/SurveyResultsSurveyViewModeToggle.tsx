e74d628a09c17b5791a6054841d3c2fca783e150
Agalmic <agalmicsoftware@protonmail.com>
2026-05-27 13:13:30 -0700

    refactor: extract survey results view mode toggle

---
+++ client/src/components/SurveyTool/SurveyResultsSurveyViewModeToggle.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Label } from 'reactstrap';
+
+type SurveyResultsSurveyViewModeToggleProps = {
+  isAggregate?: boolean;
+  knobStyle?: React.CSSProperties;
+  onKeyDown: React.KeyboardEventHandler<HTMLElement>;
+  onToggle: () => void;
+  styleMap: Record<string, string>;
+  trailingLabelStyle?: React.CSSProperties;
+};
+
+const SurveyResultsSurveyViewModeToggle = ({
+  isAggregate = false,
+  knobStyle,
+  onKeyDown,
+  onToggle,
+  styleMap,
+  trailingLabelStyle,
+}: SurveyResultsSurveyViewModeToggleProps): React.ReactElement => (
+  <div className={styleMap.surveyViewModeToggle}>
+    <Label className={styleMap.toggleLabel}>Individual</Label>
+    <div
+      className={styleMap.toggleSwitch}
+      role="switch"
+      aria-label="Toggle between individual and aggregate view"
+      aria-checked={isAggregate}
+      tabIndex={0}
+      onClick={onToggle}
+      onKeyDown={onKeyDown}
+    >
+      <div
+        className={styleMap.toggleKnob}
+        style={knobStyle}
+      />
+    </div>
+    <Label className={styleMap.toggleLabel} style={trailingLabelStyle}>
+      Aggregate
+    </Label>
+  </div>
+);
+
+export default SurveyResultsSurveyViewModeToggle;
