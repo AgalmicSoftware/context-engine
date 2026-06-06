5c2f839d7a9390bfc52facc14d9d01fd051b222d
Agalmic <agalmicsoftware@protonmail.com>
2026-06-05 22:02:35 -0700

    refactor: pass survey questions authoring display state

--- client/src/components/SurveyTool/SurveyQuestionsAuthoringPanel.test.tsx
+++ client/src/components/SurveyTool/SurveyQuestionsAuthoringPanel.test.tsx
@@ -36,8 +36,10 @@ describe('SurveyQuestionsAuthoringPanel', () => {
     render(
       <SurveyQuestionsAuthoringPanel
         {...baseProps}
-        showBackToTopControl
-        showJsonControl
+        displayState={{
+          showBackToTopControl: true,
+          showJsonControl: true,
+        }}
       />
     );
 
@@ -63,8 +65,10 @@ describe('SurveyQuestionsAuthoringPanel', () => {
     rerender(
       <SurveyQuestionsAuthoringPanel
         {...baseProps}
+        displayState={{
+          showLockedQuestionsBanner: true,
+        }}
         lockedQuestionsBanner={<div data-testid="locked-banner">Locked prompts</div>}
-        showLockedQuestionsBanner
         submittedResponseView={null}
       />
     );
