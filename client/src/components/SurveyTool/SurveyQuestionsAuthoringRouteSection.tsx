28590c8dd588c5d33332d02abf63bac50ace8e31
Agalmic <agalmicsoftware@protonmail.com>
2026-06-06 18:21:56 -0700

    refactor: extract survey questions authoring route section

--- client/src/components/SurveyTool/SurveyQuestionsAuthoringRouteSection.tsx
+++ client/src/components/SurveyTool/SurveyQuestionsAuthoringRouteSection.tsx
@@ -3,9 +3,57 @@ import React from 'react';
 import SurveyQuestionsAuthoringPanel from './SurveyQuestionsAuthoringPanel';
 import SurveyQuestionsSubmittedResponseView from './SurveyQuestionsSubmittedResponseView';
 import SurveyQuestionsSubmitFooter from './SurveyQuestionsSubmitFooter';
-import type { SurveyQuestionsAuthoringRouteSectionProps } from './surveyQuestionsRouteSurfaceTypes.js';
+import type {
+  SurveyQuestionsAuthoringPanelDisplayState,
+  SurveyQuestionsSubmitFooterDisplayState,
+} from './surveyQuestionsTypes.js';
 
 const noop = () => {};
+
+type SurveyQuestionsRouteAuthoringPanelProps = {
+  displayState?: Partial<SurveyQuestionsAuthoringPanelDisplayState>;
+  lockedQuestionsBanner?: React.ReactNode;
+  onScrollToTop?: () => void;
+  onShowJsonAtBottom?: () => void;
+  renderedEditableQuestions?: React.ReactNode;
+};
+
+type SurveyQuestionsRouteSubmittedResponseViewProps = {
+  isOwnResponse?: unknown;
+  isVisible?: boolean;
+  questionPool?: any[];
+  questionPoolReady?: unknown;
+  renderQuestionAnswer?: (
+    question: any,
+    answers: any,
+    index: number,
+    isOwnResponse: unknown
+  ) => React.ReactNode;
+  renderSurveyAnswers?: (
+    responses: any[],
+    isOwnResponse: unknown
+  ) => React.ReactNode;
+  singleQuestionMode?: unknown;
+  userAnswers?: any;
+};
+
+type SurveyQuestionsRouteSubmitFooterProps = {
+  isSingleQuestionView?: boolean;
+  isSubmitting?: boolean;
+  onPrimarySubmitClick?: () => void;
+  onRevertPendingChanges?: () => void;
+  pendingEditCount?: number;
+  responseUrl?: string;
+  submitButtonText?: React.ReactNode;
+  submissionError?: string;
+};
+
+type SurveyQuestionsAuthoringRouteSectionProps = {
+  authoringPanelProps?: SurveyQuestionsRouteAuthoringPanelProps;
+  submittedResponseViewProps?: SurveyQuestionsRouteSubmittedResponseViewProps;
+  submitDisplayState?: Partial<SurveyQuestionsSubmitFooterDisplayState>;
+  submitFooterProps?: SurveyQuestionsRouteSubmitFooterProps;
+};
 
 const SurveyQuestionsAuthoringRouteSection = ({
   authoringPanelProps = {},
