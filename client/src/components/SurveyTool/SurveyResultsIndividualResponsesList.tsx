35c63dc94af6d2684ede9acce9efca0c5c9f92ca
Agalmic <agalmicsoftware@protonmail.com>
2026-06-06 19:29:54 -0700

    refactor: extract survey results individual response card

--- client/src/components/SurveyTool/SurveyResultsIndividualResponsesList.tsx
+++ client/src/components/SurveyTool/SurveyResultsIndividualResponsesList.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { Card, CardBody, CardHeader, Collapse } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCaretDown,
-  faCaretUp,
-  faExternalLinkAlt,
-} from '@fortawesome/free-solid-svg-icons';
 
-import { getShortenedAddress } from 'utilities/ui/displayHelpers.js';
-
-type SurveyResultsResponseListEntry = {
-  responder: string;
-  surveyId?: unknown;
-} & Record<string, unknown>;
+import SurveyResultsIndividualResponseCard, {
+  type SurveyResultsResponseListEntry,
+} from './SurveyResultsIndividualResponseCard';
 
 type SurveyResultsIndividualResponsesListProps = {
   activeToggles?: Record<number, unknown>;
@@ -39,44 +29,19 @@ const SurveyResultsIndividualResponsesList = ({
     {responses.length === 0 && !filterLoading ? (
       <p>No results yet.</p>
     ) : (
-      responses.map((response: SurveyResultsResponseListEntry, index: number) => {
-        const openToggle = !!activeToggles[index];
-        return (
-          <Card key={index} className={styleMap.singleResponseCard}>
-            <CardHeader
-              onClick={() => onToggleResponse(index)}
-              className={styleMap.responseHeader}
-            >
-              <span className={styleMap.responderAddress}>
-                <a
-                  href={`/u/${encodeURIComponent(response.responder)}`}
-                  className={styleMap.responderLink}
-                  onClick={(e: React.MouseEvent<HTMLAnchorElement>) => e.stopPropagation()}
-                >
-                  {getShortenedAddress(response.responder, false)}
-                </a>
-                <a
-                  href={`/survey/${encodeURIComponent(currentSurveyId)}/${encodeURIComponent(response.responder)}${effectiveSlug ? `?session=${encodeURIComponent(effectiveSlug)}` : ''}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styleMap.externalLink}
-                >
-                  <FontAwesomeIcon icon={faExternalLinkAlt} />
-                </a>
-              </span>
-              <FontAwesomeIcon
-                icon={openToggle ? faCaretUp : faCaretDown}
-                className={styleMap.biggerIcon}
-              />
-            </CardHeader>
-            <Collapse isOpen={openToggle} id={styleMap.surveyResultsCollapse}>
-              <CardBody className={styleMap.responseCard}>
-                {renderResponseBody(response, index)}
-              </CardBody>
-            </Collapse>
-          </Card>
-        );
-      })
+      responses.map((response: SurveyResultsResponseListEntry, index: number) => (
+        <SurveyResultsIndividualResponseCard
+          key={index}
+          currentSurveyId={currentSurveyId}
+          effectiveSlug={effectiveSlug}
+          index={index}
+          isOpen={!!activeToggles[index]}
+          onToggleResponse={onToggleResponse}
+          renderResponseBody={renderResponseBody}
+          response={response}
+          styleMap={styleMap}
+        />
+      ))
     )}
   </div>
 );
