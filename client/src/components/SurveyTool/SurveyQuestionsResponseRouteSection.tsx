e06b3908a6043501391e34d734229b88531c9944
Agalmic <agalmicsoftware@protonmail.com>
2026-06-06 18:23:21 -0700

    refactor: extract survey questions response route section

--- client/src/components/SurveyTool/SurveyQuestionsResponseRouteSection.tsx
+++ client/src/components/SurveyTool/SurveyQuestionsResponseRouteSection.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
 import SurveyQuestionsResponseView from './SurveyQuestionsResponseView';
-import type { SurveyQuestionsResponseRouteSectionProps } from './surveyQuestionsRouteSurfaceTypes.js';
+import type {
+  SurveyQuestionsLayoutDisplayState,
+  SurveyQuestionsRouteViewDisplayState,
+} from './surveyQuestionsTypes.js';
+
+type SurveyQuestionsRouteResponseViewProps = {
+  isLoadingResponse?: boolean;
+  noResponse?: boolean;
+  parsedViewAddressAnswers?: any;
+  questionPool?: any[];
+  questionPoolReady?: unknown;
+  renderQuestionAnswer?: (
+    question: any,
+    answers: any,
+    index: number,
+    isOwnResponse: unknown
+  ) => React.ReactNode;
+  renderSurveyAnswers?: (
+    responses: any[],
+    isOwnResponse: unknown
+  ) => React.ReactNode;
+  responderAddress?: string;
+  responseLookupWarning?: React.ReactNode;
+  singleQuestionMode?: unknown;
+  userAnswers?: any;
+  viewAddress?: string;
+};
+
+type SurveyQuestionsResponseRouteSectionProps = {
+  layoutDisplayState?: Partial<SurveyQuestionsLayoutDisplayState>;
+  responseViewProps?: SurveyQuestionsRouteResponseViewProps;
+  routeViewDisplayState?: Partial<SurveyQuestionsRouteViewDisplayState>;
+};
 
 const SurveyQuestionsResponseRouteSection = ({
   layoutDisplayState = {},
