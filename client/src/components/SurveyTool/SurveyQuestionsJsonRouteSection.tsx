317685dbab99b5d834625ab5acfb25d7cfa8ec1f
Agalmic <agalmicsoftware@protonmail.com>
2026-06-08 06:15:49 -0700

    refactor: centralize survey questions route contracts

--- client/src/components/SurveyTool/SurveyQuestionsJsonRouteSection.tsx
+++ client/src/components/SurveyTool/SurveyQuestionsJsonRouteSection.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
 
 import SurveyQuestionsJsonControls from './SurveyQuestionsJsonControls';
-import type { SurveyQuestionsJsonPanelDisplayState } from './surveyQuestionsTypes.js';
+import type { SurveyQuestionsJsonRouteSectionProps } from './surveyQuestionsRouteSurfaceTypes.js';
 
 const noop = () => {};
-
-type SurveyQuestionsJsonRouteSectionProps = {
-  bottomRef?: React.Ref<HTMLDivElement>;
-  copiedQuestionsJson?: boolean;
-  copiedResponseJson?: boolean;
-  copiedSurveyJson?: boolean;
-  hidden?: boolean;
-  jsonPanelDisplayState?: Partial<SurveyQuestionsJsonPanelDisplayState>;
-  onCopyQuestionsJson?: () => void;
-  onCopyResponseJson?: () => void;
-  onCopySurveyJson?: () => void;
-  onToggleQuestionsJson?: () => void;
-  onToggleResponseJson?: () => void;
-  onToggleSurveyJson?: () => void;
-  questionsJson?: unknown;
-  renderJsonTree?: (json: unknown) => React.ReactNode;
-  responseJson?: unknown;
-  surveyJson?: unknown;
-};
 
 const SurveyQuestionsJsonRouteSection = ({
   bottomRef,
