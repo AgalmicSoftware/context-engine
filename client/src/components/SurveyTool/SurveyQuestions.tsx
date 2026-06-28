dfd4e6901035583b8a8248b6317627f09e5850a3
Agalmic <agalmicsoftware@protonmail.com>
2026-06-28 02:16:48 -0600

    chore: keep branch type debt flat

--- client/src/components/SurveyTool/SurveyQuestions.tsx
+++ client/src/components/SurveyTool/SurveyQuestions.tsx
@@ -5742,7 +5742,7 @@ const handleDecryptQuestionAnswerInternal = async (questionId: any, fieldToDecry
     }
 
     const surveyIndex: any = context.surveyIndex;
-    const qid: any = normalizeQuestionIdKey(questionId);
+    const qid = normalizeQuestionIdKey(questionId);
     if (!qid) return false;
 
     try {
