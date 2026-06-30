6ad9f39537ac3f8c39d2a144818234f0f60143c3
Agalmic <agalmicsoftware@protonmail.com>
2026-06-29 19:16:39 -0600

    refactor: split html report section selection

---
+++ client/src/components/SurveyTool/surveyResultsHtmlReportSelection.ts
@@ -0,0 +1,18 @@
+import type {
+  SessionResultsSectionSelection,
+} from '../../utilities/sessionResultsExport';
+
+export const SURVEY_RESULTS_HTML_REPORT_DEFAULT_SELECTED_SECTIONS: Required<SessionResultsSectionSelection> = Object.freeze({
+  argumentMap: false,
+  atlas: false,
+  report: true,
+  riskMatrix: false,
+  snapshotJson: true,
+});
+
+export const normalizeSurveyResultsHtmlReportSelectedSections = (
+  selectedSections: SessionResultsSectionSelection | null | undefined
+): Required<SessionResultsSectionSelection> => ({
+  ...SURVEY_RESULTS_HTML_REPORT_DEFAULT_SELECTED_SECTIONS,
+  ...(selectedSections || {}),
+});
