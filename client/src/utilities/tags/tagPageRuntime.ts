3c2cbb8b3e302cbd0fa8f130cf33b5d73d0a19d0
Agalmic <agalmicsoftware@protonmail.com>
2026-06-30 01:35:41 -0600

    refactor: route TagPage through runtime facade

---
+++ client/src/utilities/tags/tagPageRuntime.ts
@@ -0,0 +1,8 @@
+export {
+  getAllSessionSlugs,
+  getDemoSessionConfigBySlug,
+  getSessionConfigBySlug,
+} from '../web3/contractScripts.js';
+export {
+  SESSION_REGISTRY_CACHE_UPDATED_EVENT,
+} from '../web3/sessionRegistry.js';
