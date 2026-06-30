77f93255a43c9607571a789732bc7c1708237fdd
Agalmic <agalmicsoftware@protonmail.com>
2026-06-30 02:07:52 -0600

    refactor: narrow ContractPage runtime facade

--- client/src/utilities/contracts/contractPageRuntime.ts
+++ client/src/utilities/contracts/contractPageRuntime.ts
@@ -1,5 +1,20 @@
+import contractScripts from '../web3/contractScripts.js';
+
+const contractScriptsUntyped = contractScripts as unknown as {
+  hexToBase64url: (value: string) => string;
+  base64urlToHex: (value: string) => string;
+  base64urlToBase64: (value: string) => string;
+};
+
+const contractPageRuntime = {
+  hexToBase64url: (value: string) => contractScriptsUntyped.hexToBase64url(value),
+  base64urlToHex: (value: string) => contractScriptsUntyped.base64urlToHex(value),
+  base64urlToBase64: (value: string) => contractScriptsUntyped.base64urlToBase64(value),
+};
+
+export default contractPageRuntime;
+
 export {
-  default,
   getDemoSessionConfigBySlug,
   getSessionConfigBySlug,
   getSessionConfigBySlugOrDefault,
