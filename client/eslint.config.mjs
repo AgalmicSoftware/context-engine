6f61e634a5134f35d584baaaaaf7c3f1c92fdd13
Agalmic <agalmicsoftware@protonmail.com>
2026-07-02 05:22:08 -0600

    test: lint tag page components

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -44,6 +44,7 @@ const typedDebateMapComponentFiles = ['src/components/DebateMap/**/*.{ts,tsx}'];
 const typedNavbarComponentFiles = ['src/components/Navbar/**/*.{ts,tsx}'];
 const typedContractPageComponentFiles = ['src/components/ContractPage/**/*.{ts,tsx}'];
 const typedOnePageSessionComponentFiles = ['src/components/OnePageSession/**/*.{ts,tsx}'];
+const typedTagPageComponentFiles = ['src/components/TagPage/**/*.{ts,tsx}'];
 
 const sharedLanguageOptions = {
   parser: tsParser,
@@ -356,6 +357,17 @@ export default [
   },
   {
     files: typedOnePageSessionComponentFiles,
+    languageOptions: sharedLanguageOptions,
+    plugins: reactPlugins,
+    settings: reactSettings,
+    rules: {
+      ...sharedRules,
+      ...reactRules,
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: typedTagPageComponentFiles,
     languageOptions: sharedLanguageOptions,
     plugins: reactPlugins,
     settings: reactSettings,
