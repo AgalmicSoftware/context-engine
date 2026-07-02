f14fa09a710cb8bae576c57eae8af0170f23dff0
Agalmic <agalmicsoftware@protonmail.com>
2026-07-02 17:05:10 -0600

    refactor: add SurveyResults cache port

---
+++ client/src/domains/surveys/surveyResultsCachePort.ts
@@ -0,0 +1,97 @@
+import * as cacheScriptsModule from '../../utilities/cache/cacheScripts.js';
+
+export type SurveyResultsManagedCacheNamespace =
+  | 'questionsCache'
+  | 'surveysCache'
+  | 'bookmarksCache'
+  | 'filters'
+  | 'sbtCache'
+  | 'userCache'
+  | 'analysisCache';
+
+export type SurveyResultsCachePeekOptions = {
+  clone?: boolean;
+};
+
+export type SurveyResultsCacheListOptions = {
+  cloneValues?: boolean;
+};
+
+export type SurveyResultsCacheEntry = {
+  key?: string;
+  namespace?: string;
+  slug: string;
+  value: unknown;
+};
+
+export type SurveyResultsCacheUpdate = {
+  action?: string;
+  key?: string;
+  namespace?: string;
+  slug?: string;
+  source?: string;
+  value?: unknown;
+};
+
+export type SurveyResultsCacheUpdateHandler = (update: SurveyResultsCacheUpdate) => void;
+export type SurveyResultsCacheUnsubscribe = () => void;
+
+export type SurveyResultsCacheScriptsModule = {
+  listNamespaceEntriesSync: (
+    namespace: SurveyResultsManagedCacheNamespace | string,
+    options?: SurveyResultsCacheListOptions
+  ) => SurveyResultsCacheEntry[];
+  peekCacheSync: (
+    namespace: SurveyResultsManagedCacheNamespace | string,
+    slug?: string,
+    options?: SurveyResultsCachePeekOptions
+  ) => unknown;
+  readCache: (
+    namespace: SurveyResultsManagedCacheNamespace | string,
+    slug?: string
+  ) => Promise<unknown>;
+  subscribeCacheUpdates: (
+    handler: SurveyResultsCacheUpdateHandler
+  ) => SurveyResultsCacheUnsubscribe;
+  writeCache: (
+    namespace: SurveyResultsManagedCacheNamespace | string,
+    slug?: string,
+    value?: unknown
+  ) => Promise<unknown>;
+};
+
+export type SurveyResultsCachePort = SurveyResultsCacheScriptsModule;
+
+export type BindSurveyResultsCachePortArgs = {
+  cacheScripts: () => SurveyResultsCacheScriptsModule;
+};
+
+export const bindSurveyResultsCachePort = ({
+  cacheScripts: readCacheScripts,
+}: BindSurveyResultsCachePortArgs): SurveyResultsCachePort => ({
+  listNamespaceEntriesSync: (namespace, options) => (
+    options === undefined
+      ? readCacheScripts().listNamespaceEntriesSync(namespace)
+      : readCacheScripts().listNamespaceEntriesSync(namespace, options)
+  ),
+  peekCacheSync: (namespace, slug, options) => {
+    if (slug === undefined) return readCacheScripts().peekCacheSync(namespace);
+    if (options === undefined) return readCacheScripts().peekCacheSync(namespace, slug);
+    return readCacheScripts().peekCacheSync(namespace, slug, options);
+  },
+  readCache: (namespace, slug) => (
+    slug === undefined
+      ? readCacheScripts().readCache(namespace)
+      : readCacheScripts().readCache(namespace, slug)
+  ),
+  subscribeCacheUpdates: (handler) => readCacheScripts().subscribeCacheUpdates(handler),
+  writeCache: (namespace, slug, value) => {
+    if (slug === undefined) return readCacheScripts().writeCache(namespace);
+    if (value === undefined) return readCacheScripts().writeCache(namespace, slug);
+    return readCacheScripts().writeCache(namespace, slug, value);
+  },
+});
+
+export const surveyResultsCachePort = bindSurveyResultsCachePort({
+  cacheScripts: () => cacheScriptsModule,
+});
