7e1ec235bec31ee2faa004f80d68b968ff4dbd54
Agalmic <agalmicsoftware@protonmail.com>
2026-07-02 01:26:22 -0600

    refactor: route session media URL domain

---
+++ client/src/domains/sessions/sessionMediaUrls.ts
@@ -0,0 +1,11 @@
+import { normalizeArweaveUrl } from '../../utilities/arweave/arweaveUrls.js';
+
+export type SessionMediaUrlOptions = {
+  gateway?: unknown;
+  contextLabel?: unknown;
+};
+
+export const normalizeSessionMediaUrl = (
+  value: unknown,
+  options: SessionMediaUrlOptions = {}
+): string => normalizeArweaveUrl(value, options);
