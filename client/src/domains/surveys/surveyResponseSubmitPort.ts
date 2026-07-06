97348fadb7e8e1fe27f42db9db5ba11bc4828455
Agalmic <agalmicsoftware@protonmail.com>
2026-07-06 04:52:43 -0600

    refactor(autocoder): add survey runtime ports

---
+++ client/src/domains/surveys/surveyResponseSubmitPort.ts
@@ -0,0 +1,44 @@
+import contractScripts from '../../utilities/web3/contractScripts.js';
+
+export type SurveyResponseSubmitGateway = {
+  submitResponses: (
+    provider: unknown,
+    questionIds: unknown,
+    questionResponses: unknown,
+    surveyId: unknown,
+    surveyResponse: unknown,
+    submissionGroupKey: unknown
+  ) => Promise<unknown>;
+};
+
+export type SurveyResponseSubmitPort = SurveyResponseSubmitGateway;
+
+export type BindSurveyResponseSubmitPortArgs = {
+  chainGateway: () => SurveyResponseSubmitGateway;
+};
+
+export const bindSurveyResponseSubmitPort = ({
+  chainGateway: readChainGateway,
+}: BindSurveyResponseSubmitPortArgs): SurveyResponseSubmitPort => ({
+  submitResponses: (
+    provider,
+    questionIds,
+    questionResponses,
+    surveyId,
+    surveyResponse,
+    submissionGroupKey
+  ) => (
+    readChainGateway().submitResponses(
+      provider,
+      questionIds,
+      questionResponses,
+      surveyId,
+      surveyResponse,
+      submissionGroupKey
+    )
+  ),
+});
+
+export const surveyResponseSubmitPort = bindSurveyResponseSubmitPort({
+  chainGateway: () => contractScripts as SurveyResponseSubmitGateway,
+});
