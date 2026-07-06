99c42d7bfe9a9458d4a58766dc743992d1feb0ba
Agalmic <agalmicsoftware@protonmail.com>
2026-07-06 02:48:44 -0600

    refactor: route demo fallback

--- scripts/client-large-file-inventory.test.js
+++ scripts/client-large-file-inventory.test.js
@@ -14,7 +14,7 @@ const LARGE_FILE_SHRINK_TOLERANCE = 100;
 const LARGE_CLIENT_FILE_BASELINE = Object.freeze({
   'client/src/components/SurveyTool/SurveyQuestions.tsx': 8761,
   'client/src/components/MainSite/MainSite.tsx': 6736,
-  'client/src/components/SurveyTool/SurveyResults.tsx': 4418,
+  'client/src/components/SurveyTool/SurveyResults.tsx': 4360,
   'client/src/utilities/web3/contractScripts.impl.ts': 4691,
   'client/src/components/SBTs/SBTPage.tsx': 4214,
   'client/src/components/Sessions/SessionWizard.tsx': 4546,
