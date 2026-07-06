f51bd9c83d9b6bd60323c6016736a24786050eff
Agalmic <agalmicsoftware@protonmail.com>
2026-07-05 23:41:11 -0600

    refactor: extract metadata resolution helpers

--- scripts/client-large-file-inventory.test.js
+++ scripts/client-large-file-inventory.test.js
@@ -15,7 +15,7 @@ const LARGE_CLIENT_FILE_BASELINE = Object.freeze({
   'client/src/components/SurveyTool/SurveyQuestions.tsx': 8761,
   'client/src/components/MainSite/MainSite.tsx': 6736,
   'client/src/components/SurveyTool/SurveyResults.tsx': 5109,
-  'client/src/utilities/web3/contractScripts.impl.ts': 5337,
+  'client/src/utilities/web3/contractScripts.impl.ts': 4691,
   'client/src/components/SBTs/SBTPage.tsx': 4214,
   'client/src/components/Sessions/SessionWizard.tsx': 4546,
   'client/src/components/UserPage/UserPage.tsx': 3784,
